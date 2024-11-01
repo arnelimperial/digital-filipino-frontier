b48f11ae58ea8bef67242ba43752cd07464ab693
arnel <9pagemake7@gmail.com>
2024-11-01 23:55:55 +0800

    :sparkles: (project) complete all sections

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Digital Filipino Frontier';
-export const SITE_DESCRIPTION = 'Reviving the Pulse of a Decade: Explore Philippine Popular Culture 2000-2010!';
+export const SITE_DESCRIPTION = 'Reviving the Pulse of a Decade: Explore Philippine Popular Culture!';
